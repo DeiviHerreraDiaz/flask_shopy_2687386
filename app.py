3b184632207ee4db794d215612810f9f94aaded8
DeiviHerreraDiaz <128943900+DeiviHerreraDiaz@users.noreply.github.com>
2023-08-11 08:59:50 -0500

    Realizado papá

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config  import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -14,11 +15,31 @@ migrate = Migrate(app, db)
 
 # Modelos - entidades del proyecto
 class Cliente(db.Model):
-    __tablename__ = "Clientes"
+    __tablename__ = "clientes"
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100), unique = True)
     password = db.Column(db.String(50))
     email = db.Column(db.String(100), unique = True)
+    
+class Producto(db.Model):
+    __tablename__ = "productos"
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(100), unique = True)
+    precio = db.Column(db.Numeric( precision = 10, scale = 2))
+    imagen = db.Column(db.String(120), unique = True)
+    
+class Venta(db.Model):
+    __tablename__ = "ventas"
+    id = db.Column(db.Integer, primary_key = True)
+    fecha = db.Column(db.DateTime, default = datetime.utcnow)
+    cliente_id = db.Column(db.Integer, db.ForeignKey('clientes.id'))
+
+class Detalles(db.Model):
+    _tablename_ = "detalles"
+    id = db.Column(db.Integer, primary_key = True)
+    producto_id = db.Column(db.Integer, db.ForeignKey('productos.id'))
+    venta_id = db.Column(db.Integer, db.ForeignKey('ventas.id'))
+    cantidad = db.Column(db.Integer)
     
 # class ():
 #     hola = str
