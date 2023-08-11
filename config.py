c08c690bf95cc987b1c3092e2ccdb923c71d67cf
Deyby <arizapaul987@gmail.com>
2023-08-11 07:58:38 -0500

    Main commit

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:1234@localhost/flask_shopy_2687386"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+#localhost/flask_shopy_2687386
